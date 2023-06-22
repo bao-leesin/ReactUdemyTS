0cbf872b3d8859473d75966d171b48e4d0efb797
Baoists <haduytuanbao@gmail.com>
2023-06-22 17:39:22 +0700

    setup

--- src/components/FatherComponent.tsx
+++ src/components/FatherComponent.tsx
@@ -1,43 +1,25 @@
 interface Testing{
     attribute1 : string,
-    attribute2: number
+    attribute2: number,
+    children?: JSX.Element
 }
 
 function Test(params:Testing) {
-   return <h3>{params.attribute1}</h3>
+   return( 
+    <>
+   <h3>{params.attribute1}</h3>
+   {params.children}
+    </>
+   )
 }
 
-function Cover() {
+export default function Cover() {
     return (
         <>
-        <Test {...{attribute1: "ss", attribute2: 5}}/>
+        <Test {...{attribute1: "ss", attribute2: 5}}>
+       <button>HGSHGFHS</button>
+       </Test>
         </>
     )
 }
 
-interface EmployeeProps {
-    name: string;
-    age: number;
-    country: string;
-  }
-  
-  function Employee({name, age, country}: EmployeeProps) {
-    return (
-      <div>
-        <h2>{name}</h2>
-        <h2>{age}</h2>
-        <h2>{country}</h2>
-      </div>
-    );
-  }
-  
-  export default function App() {
-    const obj = {name: 'Alice', age: 29, country: 'Austria'};
-  
-    return (
-      <div>
-        <Employee {...obj} />
-      </div>
-    );
-  }
-  
