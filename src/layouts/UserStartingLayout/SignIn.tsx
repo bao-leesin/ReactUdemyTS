c130e82da43d10c83155c17a7984ffe90401e775
mr.B <haduytuanbao@gmail.com>
2023-06-26 23:00:08 +0700

    ...

--- src/layouts/UserStartingLayout/SignIn.tsx
+++ src/layouts/UserStartingLayout/SignIn.tsx
@@ -1,10 +1,11 @@
+import { PortraitOutlined } from "@mui/icons-material";
 import { Avatar, Box, FormControl, Grid } from "@mui/material";
 
 
 export default function SignIn() {
 return (
 <Box>
-
+<Avatar> <PortraitOutlined/> </Avatar>
 </Box>
 )    
 }
