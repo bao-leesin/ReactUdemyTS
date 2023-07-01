e6bf8bce35d02b8536b98d25da16cd3751241570
Bao at hometown <haduytuanbao@gmail.com>
2023-07-01 10:58:20 +0700

    home

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import CssBaseline from "@mui/material/CssBaseline"
+import UserStartingLayout from "./layouts/UserStartingLayout/UserStartingLayout"
+import { useState } from "react"
+
+
+<meta name="viewport" content="initial-scale=1, width=device-width" />
 
 
 
@@ -5,7 +11,8 @@ function App() {
 
   return (
     <>
-
+    <CssBaseline/>
+    <UserStartingLayout/>
     </>
   )
 }
