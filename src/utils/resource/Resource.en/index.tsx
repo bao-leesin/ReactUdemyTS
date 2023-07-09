c528b0d0d3b6557431219a2f50fd9aa613a35649
Bao at hometown <haduytuanbao@gmail.com>
2023-07-09 16:52:13 +0700

    resource

---
+++ src/utils/resource/Resource.en/index.tsx
@@ -0,0 +1,4 @@
+import ErrorEn from "./Error.en.json"
+import LabelEn from "./Label.en.json"
+
+export default {ErrorEn,LabelEn}
---
+++ src/utils/resource/Resource.vn/index.tsx
@@ -0,0 +1,5 @@
+import ErrorVn  from "./Error.vn.json";
+import LabelVn  from "./Label.vn.json";
+
+export default {ErrorVn,LabelVn}
+
